c508813a05e9d5de7c516475ce9556bb6b24677c
Graham Jenner <grahamjenner84@googlemail.com>
2026-05-26 22:46:54 +0100

    Fix sitemap domain

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'http://localhost:3000',
+      url: 'http://footballparent.co.uk',
       lastModified: new Date(),
     },
   ]
